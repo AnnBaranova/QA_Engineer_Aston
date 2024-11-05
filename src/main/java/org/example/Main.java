982e7603104cfa46e303cc161513a6c033ecbed7
Anna Baranova <baranova.av@yandex.ru>
2024-11-05 14:45:00 +0700

    Домашнее задание по Лекции 14 (вычисление факториала, тесты будут в отдельных ветках)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,19 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("факториал 3 = " + factorial(3));
+    }
+    public static int factorial(int n) {
+        if (n < 0)
+            return -1;
+
+        int result = 1;
+
+        for (; n > 0;) {
+            result = result * n;
+            n = n - 1;
+        }
+        return result;
     }
 }
+
