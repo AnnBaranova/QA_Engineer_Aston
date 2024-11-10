a7a546c638aa4e4f304897bd7a36c45e3bf30308
Anna Baranova <baranova.av@yandex.ru>
2024-11-10 22:47:12 +0700

    Домашнее задание по Лекции 14, тесты разделены

--- src/main/java/org/example/FactorialTest.java
+++ src/main/java/org/example/FactorialTest.java
@@ -3,11 +3,17 @@ package org.example;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class BaseTest {
+public class FactorialTest {
     @Test
-    public static void baseTest() {
+    public static void positiveNumbers() {
         Assert.assertEquals(Main.factorial(3), 6);
+    }
+    @Test
+    public static void zero() {
         Assert.assertEquals(Main.factorial(0), 1);
+    }
+    @Test
+    public static void negativeNumbers() {
         Assert.assertEquals(Main.factorial(-1), -1);
         Assert.assertEquals(Main.factorial(-10), -1);
     }
